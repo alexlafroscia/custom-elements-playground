fe72dc478f3ee7b1cafb6a783bca414e3c2e90b9
Alex LaFroscia <alex@lafroscia.com>
2026-06-06 13:32:14 -0400

    build a basic custom element with svelte

---
+++ definitions/svelte-vite-custom/src/SimpleVite.svelte
@@ -0,0 +1,15 @@
+<svelte:options
+  customElement={{
+    tag: "svelte-vite-custom",
+  }}
+/>
+
+<script lang="ts">
+  interface Props {
+    name: string;
+  }
+
+  let { name }: Props = $props();
+</script>
+
+<p>Hello, {name}</p>
--- definitions/svelte-vite-custom/vite.config.ts
+++ definitions/svelte-vite-custom/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from "node:path";
+
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "SvelteViteCustom",
+      fileName: "index",
+      formats: ["es"],
+    },
+  },
+});
