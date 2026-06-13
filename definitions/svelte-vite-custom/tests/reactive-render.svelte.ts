7e4cee34bd238d6b79416bd82efa26f0b6a53113
Alex LaFroscia <alex@lafroscia.com>
2026-06-13 10:09:41 -0400

    remove reference to cleanup callback after calling it

--- definitions/svelte-vite-custom/tests/reactive-render.svelte.ts
+++ definitions/svelte-vite-custom/tests/reactive-render.svelte.ts
@@ -1,18 +1,19 @@
 import type { TemplateResult } from "lit";
 import { tick } from "svelte";
-import { beforeEach } from "vitest";
+import { afterEach } from "vitest";
 import { type RenderResult, render as normalRender } from "vitest-browser-lit";
 
 let cleanup: (() => void) | undefined;
 
-beforeEach(() => {
+afterEach(() => {
   cleanup?.();
+  cleanup = undefined;
 });
 
 /**
  * Renders a `lit` template that automatically re-renders based on Svelte's `$effect` system
  *
- * This allows you to pass a `$state()` run into a `lit` template to dynamically
+ * This allows you to pass a `$state()` rune into a `lit` template to dynamically
  * change properties over time
  */
 export async function render(template: () => TemplateResult): Promise<RenderResult> {
