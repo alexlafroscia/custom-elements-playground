5f3784f375edf6714b8350ef51e5f66260e4438e
Alex LaFroscia <alex@lafroscia.com>
2026-06-13 09:22:03 -0400

    fix CEM build config

--- definitions/svelte-vite-custom/custom-elements-manifest.config.js
+++ definitions/svelte-vite-custom/custom-elements-manifest.config.js
@@ -1,12 +1,14 @@
-import { svelte, overrideModuleCreation } from "@ce-ts-playground/cem-plugin-svelte";
+import { createSveltePlugin } from "@ce-ts-playground/cem-plugin-svelte";
 
 import svelteConfig from "./svelte.config.js";
+
+const { plugin: svelte, overrideModuleCreation } = createSveltePlugin({
+  compilerOptions: svelteConfig.compilerOptions,
+});
 
 /** @type {import("@custom-elements-manifest/analyzer").Config} */
 export default {
   globs: ["./src/**/*.svelte"],
-  plugins: [svelte()],
-  overrideModuleCreation: overrideModuleCreation({
-    compilerOptions: svelteConfig.compilerOptions,
-  }),
+  plugins: [svelte],
+  overrideModuleCreation,
 };
